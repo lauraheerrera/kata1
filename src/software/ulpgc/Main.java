1f6dc712e73381c9a42cb4338d51913126c5da0c
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2024-10-09 23:42:20 +0100

    Main and Person created

---
+++ src/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Laura", LocalDate.of(2004,8,7)));
+    }
+}
